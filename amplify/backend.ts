dabb8908e7d3dd0d87d819d65c028ed6e73a9457
cprasongtham <cprasongtham@snaplogic.com>
2024-12-03 14:41:31 +0700

    test

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,25 +1,19 @@
-import { defineBackend, secret, FunctionHandler } from "@aws-amplify/backend";
+import { defineBackend, secret } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
 const mySecret = secret("SECRET_A");
 
-// Define the handler function explicitly
-const customHandler: FunctionHandler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ secretValue: mySecret }),
-  };
-};
-
 defineBackend({
   auth,
   data,
-  custom: {
-    name: "CustomHandler",
-    handler: customHandler,
+  custom: async () => {
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ secretValue: mySecret }),
+    };
   },
 });
